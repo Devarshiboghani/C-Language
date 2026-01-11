9d07585b2ebbf2a5ca3dc9227f4931d631d574f2
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-01-11 13:31:59 +0530

    initialize project

--- c program exercise/50_program.c
+++ c program exercise/50_program.c
@@ -4,12 +4,12 @@ int main()
 {
     float celsius = 0, kelvin = 0;
 
-    printf("Enter number of celsius = ");
+    printf("Enter temperature in celsius = ");
     scanf("%f", &celsius);
 
    kelvin = celsius + 273.15;
 
-   printf("%.0f celsius = %.2f kelvin", celsius, kelvin);
+   printf("%.2f celsius = %.2f kelvin", celsius, kelvin);
    
     return 0;
 }
