8146236ab29c9c9610ee07d13d31356e2f2eb567
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-01-11 19:52:30 +0530

    initialize project

--- if....else exercise/2_int_30_or_not.c
+++ if....else exercise/2_int_30_or_not.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int a = 0, b = 0;
+    int a, b;
 
     printf("Enter number of value 1 = ");
     scanf("%d", &a);
@@ -17,7 +17,7 @@ int main()
 
     else
     {
-        printf("%d",a+b);
+        printf("false");
     }
 
     return 0;
