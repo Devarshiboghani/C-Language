d0a6e33252ba3f9c614d1ee04cca0999bf3352e8
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-01-12 10:39:37 +0530

    initialize project

--- if....else exercise/salary.c
+++ if....else exercise/salary.c
@@ -4,30 +4,30 @@ int main()
 {
     float basic, gross, hra, da;
 
-    printf("Enter basic salary of employee = ");
+    printf("Enter basic salary = ");
     scanf("%f",&basic);
 
     if(basic <= 10000)
     {
-        hra = basic * 0.2;
-        da = basic * 0.8;
+        hra = basic * 0.20;
+        da = basic * 0.80;
     }
 
     else if(basic <= 20000)
     {
         hra = basic * 0.25;
-        da = basic * 0.9;
+        da = basic * 0.90;
     }
 
     else
     {
-        hra = basic * 0.3;
+        hra = basic * 0.30;
         da =  basic * 0.95;
     }
 
      gross = basic + hra + da;
      
-    printf("gross salary of employee = %.2f", gross);
+    printf("gross salary = %.2f", gross);
 
     return 0;
 }
